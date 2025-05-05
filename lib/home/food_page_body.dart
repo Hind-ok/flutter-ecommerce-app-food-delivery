d53e940a5064210b83c050814821c46098e1a1a4
Hind__amr <hindamraoui543@gmail.com>
2025-05-05 23:51:27 +0200

    complete slide

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -16,6 +16,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
   PageController pageController = PageController(viewportFraction: 0.85);
   var _currPageValue=0.0;
   double _scaleFactor=0.8;
+  double _height=220;
   @override
   void initState(){
     super.initState();
@@ -50,12 +51,25 @@ class _FoodPageBodyState extends State<FoodPageBody> {
     Matrix4 matrix = new Matrix4.identity();
     if(index==_currPageValue.floor()){
       var currScale = 1-(_currPageValue-index)*(1-_scaleFactor);
-      matrix = Matrix4.diagonal3Values(1, currScale, 1);
+      var currTrans = _height*(1-currScale)/2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, currTrans, 0);
+
     }else if(index == _currPageValue.floor()+1){
       var currScale = _scaleFactor +(_currPageValue-index+1)*(1-_scaleFactor);
+      var currTrans = _height*(1-currScale)/2;
       matrix = Matrix4.diagonal3Values(1, currScale, 1);
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, currTrans, 0);
+
+    }else if(index == _currPageValue.floor()-1){
+      var currScale = 1-(_currPageValue-index)*(1-_scaleFactor);    var currTrans = _height*(1-currScale)/2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1);
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, currTrans, 0);
+
+    }else{
+      var currScale=0.8;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, _height*(1-_scaleFactor)/2, 1);
     }
-    
+
     return Transform(
       transform: matrix,
       child: Stack(
@@ -80,9 +94,23 @@ class _FoodPageBodyState extends State<FoodPageBody> {
               height:120,
               margin: EdgeInsets.only(left: 30,right: 30, bottom: 35  ),
               decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30),
+                  borderRadius: BorderRadius.circular(20),
                   color: Colors.white,
-      
+                  boxShadow: [
+                    BoxShadow(
+                      color: Color(0xFFe8e8e8),
+                          blurRadius: 5.0,
+                      offset: Offset(0, 5)
+                    ),
+                    BoxShadow(
+                      color: Colors.white,
+                      offset: Offset(-5, 0),
+                    ),
+                    BoxShadow(
+                        color: Colors.white,
+                        offset: Offset(5, 0)
+                    ),
+                  ]
               ),
               child: Container(
                 padding: EdgeInsets.only(top: 15, left: 15 , right: 15),
