c6f8cc0b7d6318064e01730c01cf6765076e992c
Hind__amr <hindamraoui543@gmail.com>
2025-05-06 01:15:59 +0200

    add Dynamic MediaQuery

--- lib/widgets/big_text.dart
+++ lib/widgets/big_text.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/cupertino.dart';
+import 'package:food_delivery/utils/dimensions.dart';
 
 class BigText extends StatelessWidget {
    Color? color;
   final String text;
   double size;
   TextOverflow overFlow;
-
   BigText({Key? key, this.color = const Color(0xFF332d2b),
     required this.text,
-    this.size=20,
+    this.size=0,
     this.overFlow=TextOverflow.ellipsis
   }) : super(key: key);
 
@@ -21,7 +21,7 @@ class BigText extends StatelessWidget {
       style: TextStyle(
         fontFamily: 'Roboto',
         color: color,
-        fontSize: size,
+        fontSize: size==0?Dimensions.font20 : size,
         fontWeight: FontWeight.w400
       ),
     );
