872fc3185b87942c0af8466ef2da0b0c03a6704a
Hind__amr <hindamraoui543@gmail.com>
2025-05-05 19:28:08 +0200

    update

--- lib/home/main_food_page.dart
+++ lib/home/main_food_page.dart
@@ -4,6 +4,8 @@ import 'package:food_delivery/utils/colors.dart';
 import 'package:food_delivery/widgets/big_text.dart';
 import 'package:food_delivery/widgets/small_text.dart';
 
+import 'food_page_body.dart';
+
 class MainFoodPage extends StatefulWidget {
   const MainFoodPage({super.key});
 
@@ -50,7 +52,8 @@ class _MainFoodPageState extends State<MainFoodPage> {
                 ],
               ),
             ),
-          )
+          ),
+          FoodPageBody(),
         ],
       ),
     );
