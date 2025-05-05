9979b8fd8c0afb397a8d994c2743ffce16965052
Hind__amr <hindamraoui543@gmail.com>
2025-05-05 17:16:13 +0200

    Premier commit de mon app

---
+++ lib/home/main_food_page.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:food_delivery/utils/colors.dart';
+import 'package:food_delivery/widgets/big_text.dart';
+
+class MainFoodPage extends StatefulWidget {
+  const MainFoodPage({super.key});
+
+  @override
+  State<MainFoodPage> createState() => _MainFoodPageState();
+}
+
+class _MainFoodPageState extends State<MainFoodPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+
+            child: Container(
+              margin: EdgeInsets.only(top: 45,bottom: 15),
+              padding: EdgeInsets.only(left:20 ,right: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      BigText(text: "Morocco", color: AppColors.mainColor),
+                      Text("City")
+                    ],
+                  ),
+                  Center(
+                   child: Container(
+                    width: 45,
+                    height: 45,
+                    child: Icon(Icons.search, color: Colors.white),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color:AppColors.mainColor,
+                    ),
+                  )
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
