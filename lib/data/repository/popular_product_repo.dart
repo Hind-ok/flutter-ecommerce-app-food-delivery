d33f585209d449b269adcbde54b84b211c67e175
Hind__amr <hindamraoui543@gmail.com>
2025-05-07 01:58:42 +0200

    test sur postman done

--- lib/data/repository/popular_product_repo.dart
+++ lib/data/repository/popular_product_repo.dart
@@ -3,10 +3,9 @@ import 'package:get/get.dart';
 
 class PopularProductRepo extends GetxService {
   final ApiClient apiClient;
-
   PopularProductRepo({required this.apiClient});
 
   Future<Response> getPopularProductList() async {
-    return await apiClient.getData("https://www.dbestech.com/api/product/list");
+    return await apiClient.getData("http://mvs.bslmeiyu.com/api/v1/products/popular");
   }
 }
--- lib/helper/dependencies.dart
+++ lib/helper/dependencies.dart
@@ -1,3 +1,14 @@
-Future<void> init()async {
+import 'package:food_delivery/data/api/api_client.dart';
+import 'package:food_delivery/data/repository/popular_product_repo.dart';
+import 'package:get/get.dart';
+import '../controllers/popular_product_controller.dart';
+Future<void> init() async {
+  // API Client
+  Get.lazyPut(() => ApiClient(appBaseUrl: "https://www.dbestech.com"));
 
+  // Repositories
+  Get.lazyPut(() => PopularProductRepo(apiClient: Get.find()));
+
+  // Controllers
+  Get.lazyPut(() => PopularProductController(popularProductRepo: Get.find()));
 }
