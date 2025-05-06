4a879c896f5f480093509188af43d30fddb78d8e
Hind__amr <hindamraoui543@gmail.com>
2025-05-06 02:40:35 +0200

    add scrollable Content body

--- lib/home/main_food_page.dart
+++ lib/home/main_food_page.dart
@@ -57,7 +57,9 @@ class _MainFoodPageState extends State<MainFoodPage> {
             ),
           ),
           //showing the body
-          FoodPageBody(),
+          Expanded(child: SingleChildScrollView(
+            child: FoodPageBody(),
+          )),
         ],
       ),
     );
