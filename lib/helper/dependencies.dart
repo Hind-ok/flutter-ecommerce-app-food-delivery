d3e1769377da15dc636dd47d08a3b783218a0a39
Hind__amr <hindamraoui543@gmail.com>
2025-05-07 15:42:47 +0200

    adding for   Get.lazyPut(() => RecommendedProductController(recommendedProductRepo:Get.find()));

--- lib/helper/dependencies.dart
+++ lib/helper/dependencies.dart
@@ -3,13 +3,18 @@ import 'package:food_delivery/data/repository/popular_product_repo.dart';
 import 'package:food_delivery/utils/app_constants.dart';
 import 'package:get/get.dart';
 import '../controllers/popular_product_controller.dart';
+import '../controllers/recommended_product_controller.dart';
+import '../data/repository/recommended_product_repo.dart';
 Future<void> init() async {
   // API Client
   Get.lazyPut(() => ApiClient(appBaseUrl: AppConstants.BASE_URL));
 
   // Repositories
   Get.lazyPut(() => PopularProductRepo(apiClient:Get.find()));
+  Get.lazyPut(() => RecommendedProductRepo(apiClient:Get.find()));
 
   // Controllers
   Get.lazyPut(() => PopularProductController(popularProductRepo:Get.find()));
+  Get.lazyPut(() => RecommendedProductController(recommendedProductRepo:Get.find()));
+
 }
