f4169797b1bf63ab993cc2f23592c5fb064f7e47
Hind__amr <hindamraoui543@gmail.com>
2025-05-07 03:40:24 +0200

    add app constants

--- lib/helper/dependencies.dart
+++ lib/helper/dependencies.dart
@@ -1,10 +1,11 @@
 import 'package:food_delivery/data/api/api_client.dart';
 import 'package:food_delivery/data/repository/popular_product_repo.dart';
+import 'package:food_delivery/utils/app_constants.dart';
 import 'package:get/get.dart';
 import '../controllers/popular_product_controller.dart';
 Future<void> init() async {
   // API Client
-  Get.lazyPut(() => ApiClient(appBaseUrl: "http://mvs.bslmeiyu.com"));
+  Get.lazyPut(() => ApiClient(appBaseUrl: AppConstants.BASE_URL));
 
   // Repositories
   Get.lazyPut(() => PopularProductRepo(apiClient:Get.find()));
