53008fa202a73b6a56c6d8f259e5e4ca01cb17d8
Hind__amr <hindamraoui543@gmail.com>
2025-05-08 16:11:50 +0200

    squellete body cart page

--- lib/controllers/popular_product_controller.dart
+++ lib/controllers/popular_product_controller.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery/controllers/cart_controller.dart';
 import 'package:food_delivery/data/repository/popular_product_repo.dart';
+import 'package:food_delivery/models/cart_model.dart';
 import 'package:food_delivery/utils/colors.dart';
 import 'package:get/get.dart';
 import '../models/products_model.dart';
@@ -99,6 +100,10 @@ class PopularProductController extends GetxController {
 
   int get totalItems{
     return _cart.totalItems;
+  }
+
+  List<CartModel> get getItems{
+    return _cart.getItems;
   }
 
 }
--- lib/pages/cart/cart_page.dart
+++ lib/pages/cart/cart_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:food_delivery/utils/colors.dart';
 import 'package:food_delivery/utils/dimensions.dart';
 import 'package:food_delivery/widgets/app_icon.dart';
+import 'package:food_delivery/widgets/big_text.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
@@ -36,7 +37,58 @@ class CartPage extends StatelessWidget {
                        iconSize:  Dimensions.iconSize24,
                      ),
                    ],
-          ))
+          )),
+          Positioned(
+              top: Dimensions.height20*5,
+              left: Dimensions.width20,
+              right: Dimensions.width20,
+              bottom: 0,
+              child: Container(
+                margin: EdgeInsets.only(top: Dimensions.height15),
+                    color: Colors.red,
+                    child: MediaQuery.removePadding(
+                      context: context,
+                      removeTop: true,
+                      child: ListView.builder(
+                        itemCount: 10,
+                          itemBuilder: (_, index){
+                            return Container(
+                              width: double.maxFinite,
+                              height: Dimensions.height20*5,
+                              child: Row(
+                                children: [
+                                  Container(
+                                    width: Dimensions.height20*5,
+                                    height: Dimensions.height20*5,
+                                    margin: EdgeInsets.only(bottom: Dimensions.height10),
+                                    decoration: BoxDecoration(
+                                      image: DecorationImage(
+                                        fit: BoxFit.cover,
+                                          image: AssetImage(
+                                            "assets/image/food0.png",
+                                          )
+                                      ),
+                                      borderRadius: BorderRadius.circular(Dimensions.radius20),
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  SizedBox(width: Dimensions.width10,),
+                                  Expanded(child: Container(
+                                    height: Dimensions.height20*5,
+                                    child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                      children: [
+                                        BigText(text: "etxt",color: Colors.black54,)
+                                      ],
+                                    ),
+                                  ))
+                                ],
+                              ),
+                            );
+                          }),
+                    ),
+              ))
         ],
       ),
     );
