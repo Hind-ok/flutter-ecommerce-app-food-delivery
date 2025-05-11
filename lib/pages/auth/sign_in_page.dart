af4ab00114505b83b6173cf008115c5b96bf238b
Hind__amr <hindamraoui543@gmail.com>
2025-05-11 18:11:26 +0200

    finish build sign in page

--- lib/pages/auth/sign_in_page.dart
+++ lib/pages/auth/sign_in_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:food_delivery/pages/auth/sign_up_page.dart';
 import 'package:food_delivery/utils/colors.dart';
 import 'package:food_delivery/utils/dimensions.dart';
 import 'package:food_delivery/widgets/app_text_field.dart';
@@ -17,11 +18,7 @@ class SignInPage extends StatelessWidget {
     var passwordController = TextEditingController();
     var nameController = TextEditingController();
     var phoneController = TextEditingController();
-    var singUpImages = [
-      "t.png",
-      "f.png",
-      "g.png"
-    ];
+
     return Scaffold(
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
@@ -42,6 +39,31 @@ class SignInPage extends StatelessWidget {
                 ),
               ),
             ),
+            //welcome section
+            Container(
+              margin: EdgeInsets.only(left: Dimensions.width20),
+              width: double.maxFinite,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Hello",
+                    style: TextStyle(
+                      fontSize: Dimensions.font20*3+Dimensions.font20/2,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    "Sign into your account",
+                    style: TextStyle(
+                      fontSize: Dimensions.font20,
+                      color: Colors.grey[500],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: Dimensions.height20,),
             //Your Email
             AppTextField(textController: emailController,
                 hintText: "Email",
@@ -52,17 +74,24 @@ class SignInPage extends StatelessWidget {
                 hintText: "Password",
                 icon: Icons.password_sharp ),
             SizedBox(height: Dimensions.height20,),
-            //Your name
-            AppTextField(textController: nameController,
-                hintText: "Name",
-                icon: Icons.person ),
-            SizedBox(height: Dimensions.height20,),
-            //Your Phone
-            AppTextField(textController: phoneController,
-                hintText: "Phone",
-                icon: Icons.phone),
-            SizedBox(height: Dimensions.height20+ Dimensions.height20,),
-            //Sign up button
+            //tag line
+            Row(
+              children: [
+                Expanded(child: Container()),
+                RichText(
+                    text: TextSpan(
+                        text: "Sign into your account",
+                        style: TextStyle(
+                          color: Colors.grey[500],
+                          fontSize: Dimensions.font20,
+                        )
+                    )
+                ),
+                SizedBox(width: Dimensions.width20,)
+              ],
+            ),
+            SizedBox(height: Dimensions.screenHeight*0.05,),
+            //Sign in button
             Container(
               width: Dimensions.screenWidth/2,
               height: Dimensions.screenHeight/13,
@@ -72,47 +101,33 @@ class SignInPage extends StatelessWidget {
               ),
               child: Center(
                 child: BigText(
-                  text: "Sign Up",
+                  text: "Sign In",
                   size: Dimensions.font20 + Dimensions.font20/2,
                   color: Colors.white,
                 ),
               ),
             ),
-            SizedBox(height: Dimensions.height10,),
-            //tag line
-            RichText(
-
-                text: TextSpan(
-                    recognizer: TapGestureRecognizer()..onTap=()=>Get.back(),
-                    text: "Have an account already?",
-                    style: TextStyle(
-                      color: Colors.grey[500],
-                      fontSize: Dimensions.font20,
-                    )
-                )
-            ),
             SizedBox(height: Dimensions.screenHeight*0.05,),
             //sign up options
             RichText(
                 text: TextSpan(
-                    text: "Sign up using one of following methods",
+                    text: "Don\'t have an account?",
                     style: TextStyle(
                       color: Colors.grey[500],
-                      fontSize: Dimensions.font16,
-                    )
-                )
-            ),
-            Wrap(
-              children: List.generate(3, (index) => Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: CircleAvatar(
-                  radius: Dimensions.radius30,
-                  backgroundImage: AssetImage(
-                    "assets/image/"+singUpImages[index],
-                  ),
+                      fontSize: Dimensions.font20,
+                    ),
+                  children: [
+                    TextSpan(
+                      recognizer: TapGestureRecognizer()..onTap=()=>Get.to(()=>SignUpPage(),transition: Transition.fade),
+                    text: " Create",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: AppColors.mainBlackColor,
+                      fontSize: Dimensions.font20,
+                    ))
+                  ]
                 ),
-              )),
-            )
+            ),
           ],
         ),
       ),
